87daff4444048d8cbe7713c5dd9d7fd64c41d85c
范超 <1446486761@qq.com>
2017-08-08 09:39:36 +0800

    初始化TestPods项目

---
+++ TestPods/Classes/TestPods.h
@@ -0,0 +1,18 @@
+//
+//  TestPods.h
+//  Pods
+//
+//  Created by yitudev12 on 2017/8/8.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestPods : NSObject
+
+/**
+ 测试
+ */
+- (void)testPod;
+
+@end
---
+++ TestPods/Classes/TestPods.m
@@ -0,0 +1,20 @@
+//
+//  TestPods.m
+//  Pods
+//
+//  Created by yitudev12 on 2017/8/8.
+//
+//
+
+#import "TestPods.h"
+
+@implementation TestPods
+
+/**
+ 测试
+ */
+- (void)testPod {
+    NSLog(@"测试");
+}
+
+@end
